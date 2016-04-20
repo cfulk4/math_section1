4b2cc5e8826152b3d8970fed76a83e4d096d7271
cfulk4 <cfulk4@aol.com>
2016-04-20 09:07:09 -0400

    Update math.js

--- math.js
+++ math.js
@@ -1,16 +1,10 @@
-
-exports.add = function(a, b) {
-	return a + b;
-};
-
-exports.subtract = function(a, b) {
-	return a - b;
-};
-
-exports.multiply = function(a, b) {
-	return a * b;
-};
-
-exports.divide = function(a, b) {
-	return a / b;
-};
+#!/usr/bin/env node
+var n1 = +process.argv[3],   // "+" infront makes adding work
+    n2 = +process.argv[4],
+    operator = process.argv[2];   //operator for math 
+if (operator === 'add' || "+" === operator) {console.log (n1 + n2);}  // === is better than ==
+if (operator == 'subtract') {console.log(n1 - n2);}
+if (operator == 'multiply') {console.log(n1 * n2);}
+if (operator == 'divide') {console.log(n1 / n2);}
+//npm "node package manager" to install and uninstall
+// "npam install -g (stands for global) jasmine" If doesn't work put "sudo" in front
